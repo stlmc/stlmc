7452ad07066e65af38d03c12fc180375fcccbb94
jialee <cee5539@postech.ac.kr>
2018-07-08 13:05:10 +0900

    water without tau

--- 2.Watertank/twoWatertankNon.py
+++ 2.Watertank/twoWatertankNon.py
@@ -30,36 +30,34 @@ class Watertank(Model):
         proMF1 = Bool('promf1')
         proMF2 = Bool('promf2')
 
-        jumptau = Real('jumptau')
-
         mode = {m: (1, 4)}
-        vars = {fx: (0, 10), sx: (0, 10), jumptau: (0,1)}
-        init = And(m == RealVal(1), fx >= gH - RealVal(0.1), fx <= gH + RealVal(0.1), sx >= gH - RealVal(0.1), sx <= gH + RealVal(0.1), jumptau == RealVal(0))
+        vars = {fx: (0, 10), sx: (0, 10)}
+        init = And(m == RealVal(1), fx >= gH - RealVal(0.1), fx <= gH + RealVal(0.1), sx >= gH - RealVal(0.1), sx <= gH + RealVal(0.1))
 
         fxOff = -a * Sqrt(RealVal(2) * g) * Sqrt(fx) / A1 
         fxOn = (q1 - a * Sqrt(RealVal(2) * g) * Sqrt(fx)) / A1 
         sxOff = (a * Sqrt(RealVal(2) * g) * (Sqrt(fx) - Sqrt(sx))) / A2
         sxOn = (q2 + a * Sqrt(RealVal(2) * g) * (Sqrt(fx) - Sqrt(sx))) / A2
 
-        flow = {m == RealVal(4): {fx: fxOff, sx: sxOff, jumptau: RealVal(1)}, \
-                m == RealVal(3): {fx: fxOff, sx: sxOn, jumptau: RealVal(1)}, \
-                m == RealVal(2): {fx: fxOn, sx: sxOff, jumptau: RealVal(1)}, \
-                m == RealVal(1): {fx: fxOn, sx: sxOn, jumptau: RealVal(1)}}
+        flow = {m == RealVal(4): {fx: fxOff, sx: sxOff}, \
+                m == RealVal(3): {fx: fxOff, sx: sxOn}, \
+                m == RealVal(2): {fx: fxOn, sx: sxOff}, \
+                m == RealVal(1): {fx: fxOn, sx: sxOn}}
 
-        inv = {m == RealVal(4): And(fx > RealVal(0), sx > RealVal(0), jumptau <= RealVal(1), jumptau >= RealVal(0)), \
-               m == RealVal(3): And(fx > RealVal(0), sx <= A2, jumptau <= RealVal(1), jumptau >= RealVal(0)), \
-               m == RealVal(2): And(fx <= A1, sx > RealVal(0), jumptau <= RealVal(1), jumptau >= RealVal(0)), \
-               m == RealVal(1):  And(fx <= A1, sx <= A2, jumptau <= RealVal(1), jumptau >= RealVal(0))}
+        inv = {m == RealVal(4): And(fx > RealVal(0), sx > RealVal(0)), \
+               m == RealVal(3): And(fx > RealVal(0), sx <= A2), \
+               m == RealVal(2): And(fx <= A1, sx > RealVal(0) ), \
+               m == RealVal(1):  And(fx <= A1, sx <= A2)}
 
-        jump = {And(jumptau == RealVal(1), fx < gH, sx < gH):  And(mNext == RealVal(1), fxNext == fx, sxNext == sx), \
-               And(jumptau == RealVal(1), fx < gH, sx >= gH):  And(mNext == RealVal(2), fxNext == fx, sxNext == sx), \
-               And(jumptau == RealVal(1), fx >= gH, sx < gH):  And(mNext == RealVal(3), fxNext == fx, sxNext == sx), \
-               And(jumptau == RealVal(1), fx >= gH, sx >= gH):  And(mNext == RealVal(4), fxNext == fx, sxNext == sx)} 
+        jump = {And(fx < gH, sx < gH):  And(mNext == RealVal(1), fxNext == fx, sxNext == sx), \
+               And(fx < gH, sx >= gH):  And(mNext == RealVal(2), fxNext == fx, sxNext == sx), \
+               And(fx >= gH, sx < gH):  And(mNext == RealVal(3), fxNext == fx, sxNext == sx), \
+               And(fx >= gH, sx >= gH):  And(mNext == RealVal(4), fxNext == fx, sxNext == sx)} 
 
-        goal = {m == RealVal(4): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1)), jumptau == RealVal(0)), \
-               m == RealVal(3): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1)), jumptau == RealVal(0)), \
-               m == RealVal(2): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1)), jumptau == RealVal(0)), \
-               m == RealVal(1): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1)), jumptau == RealVal(0))}
+        goal = {m == RealVal(4): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1))), \
+               m == RealVal(3): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1))), \
+               m == RealVal(2): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1))), \
+               m == RealVal(1): And(Or(fx > gH + RealVal(0.1), fx < gH - RealVal(0.1)), Or(sx > gH + RealVal(0.1), sx < gH - RealVal(0.1)))}
 
         prop = {proPF: fx <= A1, (proQF): fx < RealVal(5), (proMF1): m == RealVal(1), proMF2: m == RealVal(2), proZF: fx > RealVal(6) }
 
