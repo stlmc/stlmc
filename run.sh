863075bbd112316af2e83581cd9f8d07459dceb0
geunyeol.ryu <rgyenr2@gmail.com>
2019-07-07 00:05:04 +0900

    rewrite scripts

--- run.sh
+++
@@ -1,2 +0,0 @@
-#java -jar antlr-4.7.1-complete.jar -Dlanguage=Python3 model.g4 -no-listener -visitor 
-python3 ./src/stlMC_main.py ./src/simpleModel/twoThermostatPoly.txt
--- src/run.sh
+++
@@ -1,8 +0,0 @@
-#java -jar antlr-4.7.1-complete.jar -Dlanguage=Python3 model.g4 -no-listener -visitor 
-#python3 stlMC_main.py simpleModel/twoThermostatPoly.txt
-#python3 stlMC_main.py simpleModel/twoBatteryPoly.txt
-python3 stlMC_main.py simpleModel/twoBatteryLinear.txt
-#python3 stlMC_main.py simpleModel/twoWatertankLinear.txt
-#python3 stlMC_main.py simpleModel/railroadPoly.txt
-#python3 stlMC_main.py simpleModel/twoCarPoly.txt
-#python3 stlMC_main.py simpleModel/twoWatertankPoly.txt
