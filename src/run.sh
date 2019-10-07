3501db7da22e98cd94d28c49a4648465eca4a2e7
jialee <cee5539@postech.ac.kr>
2019-10-07 22:03:20 +0900

    make a demo model

--- src/run.sh
+++ src/run.sh
@@ -2,5 +2,6 @@
 #python3 stlMC_main.py simpleModel/singleMode.txt
 #python3 stlMC_main.py simpleModel/modeIdtest.txt
 #python3 stlMC_main.py simpleModel/oneThermostatMix.txt
-python3 stlMC_main.py simpleModel/twoThermostatLinear.txt
+#python3 stlMC_main.py simpleModel/twoThermostatLinear.txt
+python3 stlMC_main.py simpleModel/demo.txt
 #python3 stlMC_main.py simpleModel/twoThermostatPolySimple.txt
