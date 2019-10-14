c75175f584c2f387b58b6964f15eb80673b8abcb
jialee <cee5539@postech.ac.kr>
2019-10-14 19:30:10 +0900

    revise yices handler

--- src/core/yicesHandler.py
+++ src/core/yicesHandler.py
@@ -7,155 +7,156 @@ from .node import *
 
 # return a check result and the Z3 constraint size
 def checkSat(consts, logic="None"):
-    z3Consts=[z3Obj(c) for c in consts]
+    yicesConsts=[yicesObj(c) for c in consts]
     if logic != "None":
-        solver = z3.SolverFor(logic)
+        solver = yices.SolverFor(logic)
     else:
-        solver = z3.Solver()
+        solver = yices.Solver()
     
-#    target_z3_simplify = z3.simplify(z3.And(*z3Consts))
-#    solver.add(target_z3_simplify)
+#    target_yices_simplify = yices.simplify(yices.And(*yicesConsts))
+#    solver.add(target_yices_simplify)
 
-    solver.add(z3Consts)
+    solver.add(yicesConsts)
 
     solver.set("timeout", 9000000)  #timeout : 150 min
     with open("thermoLinear.smt2", 'w') as fle:
         print(solver.to_smt2(), file=fle)
 
     result = solver.check()
-    if result == z3.sat:
+    if result == yices.sat:
         m = solver.model()
     else:
         m = None
 
-    return (result, sizeAst(z3.And(*z3Consts)), m)
+    return (result, sizeAst(yices.And(*yicesConsts)), m)
 
 # return the size of the Z3 constraint
-def sizeAst(node:z3.AstRef):
+def sizeAst(node:yices.AstRef):
     return 1 + sum([sizeAst(c) for c in node.children()])
 
 
 @singledispatch
-def z3Obj(const:Node):
+def yicesObj(const:Node):
     raise NotImplementedError('Something wrong')
 
-@z3Obj.register(Constant)
+@yicesObj.register(Constant)
 def _(const):
-    op = {Type.Bool: z3.BoolVal, Type.Real: z3.RealVal, Type.Int: z3.IntVal}
+    op = {Type.Bool: Types.bool_type(), Type.Real: Types.real_type(), Type.Int: Types.int_type()}
     if const.getType() == Type.Bool:
-        value = True if const.value else False
+        value = Terms.TRUE if const.value else Terms.FALSE
+        return value
     else:
         value = str(const.value)
-    return op[const.getType()](value)
+    return Terms.constant(op[const.getType()], value)
 
-@z3Obj.register(Variable)
+@yicesObj.register(Variable)
 def _(const):
-    op = {Type.Bool: z3.Bool, Type.Real: z3.Real, Type.Int: z3.Int}
-    return  op[const.getType()](str(const.id))
+    op = {Type.Bool: Types.bool_type(), Type.Real: Types.real_type(), Type.Int: Types.int_type()}
+    return  Terms.new_uninterpreted_term(op[const.getType()], str(const.id))
 
-@z3Obj.register(Ge)
+@yicesObj.register(Ge)
 def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x >= y
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.arith_geq_atom(x, y)
 
-@z3Obj.register(Gt)
+@yicesObj.register(Gt)
 def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x > y
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.arith_gt_atom(x, y)
 
-@z3Obj.register(Le)
+@yicesObj.register(Le)
 def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x <= y
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.arith_leq_atom(x, y)
 
-@z3Obj.register(Lt)
+@yicesObj.register(Lt)
 def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x < y
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.arith_lt_atom(x, y)
 
-@z3Obj.register(Numeq)
+@yicesObj.register(Numeq)
 def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.arith_eq_atom(x, y)
+
+@yicesObj.register(Plus)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.add(x, y)
+
+@yicesObj.register(Minus)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.sub(x, y)
+
+@yicesObj.register(Pow)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.power(x, y)
+
+@yicesObj.register(Mul)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Terms.mul(x, y)
+
+@yicesObj.register(Div)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return Temrs.division(x, y)
+
+@yicesObj.register(Neg)
+def _(const):
+    x = yicesObj(const.child())
+    return Terms.neg(x)
+
+@yicesObj.register(And)
+def _(const):
+    yicesargs = [yicesObj(c) for c in const.children]
+    if len(yicesargs) < 1:
+        return True
+    elif len(yicesargs) < 2:
+        return yicesargs[0]
+    else:
+        return yices.And(yicesargs)
+
+@yicesObj.register(Or)
+def _(const):
+    yicesargs = [yicesObj(c) for c in const.children]
+    if len(yicesargs) < 1:
+        return True
+    elif len(yicesargs) < 2:
+        return yicesargs[0]
+    else:
+        return yices.Or(yicesargs)
+
+@yicesObj.register(Implies)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
+    return yices.Implies(x, y)
+
+@yicesObj.register(Beq)
+def _(const):
+    x = yicesObj(const.left())
+    y = yicesObj(const.right())
     return x == y
 
-@z3Obj.register(Plus)
+@yicesObj.register(Not)
 def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x + y
+    x = yicesObj(const.child())
+    return yices.Not(x)
 
-@z3Obj.register(Minus)
-def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x - y
-
-@z3Obj.register(Pow)
-def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x**y
-
-@z3Obj.register(Mul)
-def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x * y
-
-@z3Obj.register(Div)
-def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x / y
-
-@z3Obj.register(Neg)
-def _(const):
-    x = z3Obj(const.child())
-    return -x
-
-@z3Obj.register(And)
-def _(const):
-    z3args = [z3Obj(c) for c in const.children]
-    if len(z3args) < 1:
-        return True
-    elif len(z3args) < 2:
-        return z3args[0]
-    else:
-        return z3.And(z3args)
-
-@z3Obj.register(Or)
-def _(const):
-    z3args = [z3Obj(c) for c in const.children]
-    if len(z3args) < 1:
-        return True
-    elif len(z3args) < 2:
-        return z3args[0]
-    else:
-        return z3.Or(z3args)
-
-@z3Obj.register(Implies)
-def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return z3.Implies(x, y)
-
-@z3Obj.register(Beq)
-def _(const):
-    x = z3Obj(const.left())
-    y = z3Obj(const.right())
-    return x == y
-
-@z3Obj.register(Not)
-def _(const):
-    x = z3Obj(const.child())
-    return z3.Not(x)
-
-@z3Obj.register(Integral)
+@yicesObj.register(Integral)
 def _(const):
     result = []
     subDict = {}
@@ -172,7 +173,7 @@ def _(const):
                         pass
                     else:
                         print(str(const.ode[subvariables[j]]))
-                        raise z3constODEerror()
+                        raise yicesconstODEerror()
         substitutionExp = {}
         for i in const.ode.keys():
             substitutionExp[str(i.id)] = const.ode[i].substitution(subDict)
@@ -194,21 +195,21 @@ def _(const):
         raise FlowTypeEerror() 
 
 
-    z3result = [z3Obj(c) for c in result]
-    return z3.And(z3result) 
+    yicesresult = [yicesObj(c) for c in result]
+    return yices.And(yicesresult) 
 
-@z3Obj.register(Forall)
+@yicesObj.register(Forall)
 def _(const):
     typeList = [i.getType() for i in const.condition.getVars()]
     if not(Type.Real in typeList):
-        subCondition = z3Obj(const.condition.substitution(const.modeDict))
+        subCondition = yicesObj(const.condition.substitution(const.modeDict))
         return subCondition
     else:
-        endCond = z3Obj(const.condition.substitution(const.endDict))
-        startCond = z3Obj(const.condition.substitution(const.startDict))
-        return z3.And(endCond, startCond)
+        endCond = yicesObj(const.condition.substitution(const.endDict))
+        startCond = yicesObj(const.condition.substitution(const.startDict))
+        return yices.And(endCond, startCond)
 
-@z3Obj.register(Solution)
+@yicesObj.register(Solution)
 def _(const):
     subDict = {}
     for i in range(len(const.endList)):
@@ -222,9 +223,9 @@ def _(const):
                 if str(const.ode[subvariables[j]]) == str(RealVal(0)):
                     pass
                 else:
-                    raise z3constODEerror()
+                    raise yicesconstODEerror()
             else:
-                raise z3constODEerror()
+                raise yicesconstODEerror()
     substitutionExp = {}
     for i in const.ode.keys():
         substitutionExp[str(i.id)] = const.ode[i].substitution(subDict)
@@ -234,6 +235,6 @@ def _(const):
         keyValue = str(const.endList[i])[0:keyIndex]
         result.append(const.endList[i] == substitutionExp[keyValue])
 
-    z3result = [z3Obj(c) for c in result]
-    return z3.And(z3result)
+    yicesresult = [yicesObj(c) for c in result]
+    return yices.And(yicesresult)
 
