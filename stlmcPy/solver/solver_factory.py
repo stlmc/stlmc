1f37ac01c531d26041a1774b3a6178e3c1327a2a
geunyeol.ryu <rgyenr2@gmail.com>
2021-02-02 22:51:37 +0900

    remove hylaa

--- stlmcPy/solver/solver_factory.py
+++ stlmcPy/solver/solver_factory.py
@@ -1,7 +1,6 @@
 from stlmcPy.solver.yices import YicesSolver
 from stlmcPy.solver.z3 import Z3Solver
 from stlmcPy.solver.dreal import dRealSolver
-from stlmcPy.solver.hylaa import HylaaSolverNaive, HylaaSolverReduction, HylaaSolverUnsatCore
 
 class SolverFactory:
     def __init__(self, solver_type):
@@ -14,9 +13,3 @@ class SolverFactory:
             return YicesSolver()
         elif self.solver_type == 'dreal':
             return dRealSolver()
-        elif self.solver_type == 'hylaa':
-            return HylaaSolverNaive()
-        elif self.solver_type == 'hylaa-reduction':
-            return HylaaSolverReduction()
-        elif self.solver_type == 'hylaa-unsat-core':
-            return HylaaSolverUnsatCore()
